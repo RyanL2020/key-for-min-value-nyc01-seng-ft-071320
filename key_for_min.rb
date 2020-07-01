91f7812df202444c43021ae70eda0282809e098f
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-01 19:37:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  binding.pry 
   name_hash.detect do |key, value|
     if key == :key && value < 2
       min_value = :key
