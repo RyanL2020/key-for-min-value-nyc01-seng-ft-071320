d18a212cde9d9bc2f1dba82ebc3e802108243572
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-01 19:28:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,11 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_value = 1
   name_hash.detect do |key, value|
     if key == :key && :value < 2
       min_value = :key
     end
 end
- min_value 
+ min_value
 end
