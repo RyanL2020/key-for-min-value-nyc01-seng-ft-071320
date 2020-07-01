7085f96749d2777f4846d59465abf578b36edef3
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-01 19:28:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = 1
   name_hash.detect do |key, value|
-    if key == :key && value < 2
+    if key == :key && :value < 2
       min_value = :key
     end
 end
