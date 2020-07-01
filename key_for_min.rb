908c593fef0ddc1556a82af4525ef37ef4750d03
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-01 19:48:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,15 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  binding.pry 
-  name_hash.detect do |key, value|
-    if key == :key && value < 2
-      min_value = :key
-    end
+  binding.pry
+  name_hash.each do |key, value|
+    puts "#{key}: #{value}"
+    #if key == :key &
+    #  min_value = :key
+    #end
 end
 end
+#restaurant_menu = { "Ramen" => 3, "Dal Makhani" => 4, "Coffee" => 2 }
+#restaurant_menu.each do | item, price |
+#  puts "#{item}: $#{price}"
+#end
