97aa44ae49c74e21a814906fbecd00c7b2d6d869
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-01 20:10:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,14 @@ require 'pry'
 
 def key_for_min_value(name_hash)
   lowest_key = 0
-  lowest_value = nil
+  lowest_value = 0
   name_hash.each do |key, value|
     puts "#{key}: #{value}"
     puts "Hi"
     #if name_hash == {}
     #  nil
     #end
-    if value < lowest_value || lowest_value == nil
+    if value < lowest_value || lowest_value == 0
      lowest_value = value
      lowest_key = key
     end
