a44b1794835c490cc62e32c53771c58412d73b43
Ryan Locascio <loc.fitness1@gmail.com>
2020-07-01 19:58:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,17 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  lowest_key = nil
+  lowest_value = nil
   name_hash.each do |key, value|
     puts "#{key}: #{value}"
     puts "Hi"
-    #if key == :key &
-    #  min_value = :key
-    #end
+    if value < lowest_value
+     lowest_value = value 
+     lowest_key = key  
+    end
 end
+lowest_key
 end
 #restaurant_menu = { "Ramen" => 3, "Dal Makhani" => 4, "Coffee" => 2 }
 #restaurant_menu.each do | item, price |
